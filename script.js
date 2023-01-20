7cee58cc79d490099fd4400ec02b2d7c3ad00564
Reinaldo Coelho <scoelhor7@gmail.com>
2023-01-20 10:11:16 -0300

    formating

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
 
-
 const add = () => {
   const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
@@ -21,15 +20,6 @@ const save = () => {
 
 button.addEventListener('click', add)
 form.addEventListener('change', save)
-
-
-
-
-// const data = {
-  //   run: ['01-01', '01-02', '01-06', '01-07', '01-08', '01-09'],
-  //   takePills: ['01-03'],
-  //   journal: ['01-02']
-  // }
 
 const data = JSON.parse(localStorage.getItem('NLWSetup-habits')) || {}
 nlwSetup.setData(data)
